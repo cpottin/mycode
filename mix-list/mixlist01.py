66f65dabf15c8ce4101ace835eea4b3549e2f711
cpottin <cindy.pottin@gmail.com>
2023-01-10 17:31:53 +0000

    mixed lists update

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -10,6 +10,6 @@ print("The last item in the list (state): " + my_list[2] )
 
 iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-print("Here are the ip addresses: " + iplist[3] + " " +iplist[4])
+print("Here are the ip addresses: " + iplist[3] + " and  " +iplist[4])
 
 
