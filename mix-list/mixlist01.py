3f83e3704d8958cf3e030faa815da9c527038fd8
cpottin <cindy.pottin@gmail.com>
2023-01-10 17:24:20 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+print("The first item in the list (IP): " + my_list[0] )
+
+print("The second item in the list (port): " + str(my_list[1]) )
+
+print("The last item in the list (state): " + my_list[2] )
+
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("Here are the ip addresses: " + iplist[3] + " " +iplist[4])
+
+
