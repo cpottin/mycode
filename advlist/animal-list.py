6947dd063beec3d4abaf02660c293c1929ea614a
cpottin <cindy.pottin@gmail.com>
2023-01-10 19:31:35 +0000

    challenge 34 plus optional

---
+++ advlist/animal-list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+    animal_list = ["cat", "dog", "cow", "fox", "hog"]
+
+    print(animal_list)
+
+    print(animal_list[2])
+
+main()
+
