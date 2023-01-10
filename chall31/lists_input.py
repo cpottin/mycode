f22f987b87045d8f43a52baeadf5013247412dc1
cpottin <cindy.pottin@gmail.com>
2023-01-10 17:58:21 +0000

    challenge 31

---
+++ chall31/lists_input.py
@@ -0,0 +1,16 @@
+wordbank = ["indentation", "spaces"]
+
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+myNum = "4"
+wordbank.append(myNum)
+
+num =int(input("Please Enter a number between 0 and 20: "))
+
+student_name = tlgstudents[num]
+
+print(student_name + " always uses " + wordbank[2] + " " + wordbank[1] + " to indent.")
+
+
+
+
