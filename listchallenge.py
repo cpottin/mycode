3f61671657c6caca1a09073f2548d8203e8128b8
cpottin <cindy.pottin@gmail.com>
2023-01-10 18:09:12 +0000

    challenge 32

---
+++ listchallenge.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+heroes= ["Spiderman", "Batman", "Black Panther", "Wonder Woman", "Storm"]
+
+# PART 1
+# Print out your favorite character from this list! The output would look something like:
+# My favorite character is Black Panther!
+print("My favorite hero is " + heroes[4])
+
+# PART 2
+# Ask the user to pick a number between 1 and 100.
+# Convert the input into an integer.
+userNum = int(input("Please enter a number between 1 and 100: "))
+
+nums= [1, -5, 56, 987, 0]
+
+# PART 3
+# check out https://docs.python.org/3/library/functions.html or go to Google
+# use a built-in function to find which integer in nums is the biggest.
+# then, print out that number!
+print(max(nums))
+
