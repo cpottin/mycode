0fe1865e4f372f0dd84e729081da30b76c7836b2
cpottin <cindy.pottin@gmail.com>
2023-01-11 23:03:20 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,10 @@
+print("This code will alays execute.")
+
+def main():
+    print("This code belongs to the main function is module 1.")
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ if_name/second_module.py
@@ -0,0 +1,8 @@
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+
+#ADD this line
+first_module.main()
+
+print("Module #2 Name=", __name__)
