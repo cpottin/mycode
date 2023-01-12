0f410a2f8e4aaf522d7088917b9ba55cc64995a2
cpottin <cindy.pottin@gmail.com>
2023-01-12 17:40:48 +0000

    refactored with 2nd question

--- chall48/magic.py
+++ chall48/magic.py
@@ -1,32 +1,48 @@
 #!/usr/bin/env python3
 import random
 
-eightball_responses = ["Odds are not good", 
+print("Hello, I can tell the future. ")
+print(".............................")
+print()
+print("All you have to do is ask a question")
+
+
+def question():
+    eightball_responses = ["Odds are not good", 
             "Absolutley", "Very Likely", "It's a No"]
 
-print("Hello, I can tell the future. ")
-print("All you have to do is ask a question")
-user_question =input("What is your question? : ")
+    user_question =input("What is your question? : ")
 
-if user_question !="":
-    random_rep = random.choice(eightball_responses)
+    if user_question !="":
+        random_rep = random.choice(eightball_responses)
      
-    ##print(random_rep)
-    if random_rep == "It's a No":
-        print("I'm sorry ..." + random_rep)
-    elif random_rep == "Absolutley":
-        print("You are in luck..." + random_rep)
-    elif random_rep == "Very Likely":
-        print("Looks like it is ...." + random_rep)
+         ##print(random_rep)
+        if random_rep == "It's a No":
+            print("I'm sorry ..." + random_rep)
+        elif random_rep == "Absolutley":
+            print("You are in luck..." + random_rep)
+        elif random_rep == "Very Likely":
+            print("Looks like it is ...." + random_rep)
+        else:
+            print("Better luck next time it looks like.." + random_rep)
     else:
-        print("Better luck next time it looks like.." + random_rep)
+        print("You did not ask a questions. Please try again")
+
+question()
+
+print()
+user_secondq = input("Do you have another question? : ")
+
+if user_secondq == "yes":
+    question()
 else:
-    print("You did not ask a questions. Please try again")
+    print("Thank you for playing. Goodbye.")
 
-#user_secondq = input("Do you have another question? : ")
-
-#if user_secondq == "yes":
- #   user_question2 = input("Ask me another question")
+#count = 0
+#while count < 3:
+#    print("You have asked ")
+#    print(count)
+#    print("questions")
+#    count += 1
 #else:
- #   print("Goodbye")
-
+#    print("I need a break you have asked your max number of questions")
