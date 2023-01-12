d8f06b162e4d96e6952c2032f28add909c512c48
cpottin <cindy.pottin@gmail.com>
2023-01-12 22:04:27 +0000

    parsing files customization 2

--- attemptlogin/large_fileparse-with.py
+++ attemptlogin/large_fileparse-with.py
@@ -14,6 +14,8 @@ with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
         # if this 'fail pattern' appears in the line...
         if "- - - - -] Authorization failed" in line:
             loginfail += 1 # this is the same as loginfail = loginfail + 1
+            print(line.split(" ")[-1])
+        
         elif "-] Authorization failed" in line:
             successful_logins += 1
 print("The number of failed log in attempts is", loginfail)
