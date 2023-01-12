7b157a7bb051a18eae32df58cd2f60c345733744
cpottin <cindy.pottin@gmail.com>
2023-01-12 21:56:13 +0000

    parsing files customization 1

---
+++ attemptlogin/keystone_counter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+#set count to zero to start the count of failed attempts
+loginfail = 0
+
+#open file r = read
+keystone_file = open("/home/student/mycode/attemptlogin/keystone.common.wsgi","r")
+
+# turn the file into a list of lines in memory
+keystone_file_lines=keystone_file.readlines()
+
+#for each line loop over the list of lines
+for line in keystone_file_lines:
+        # if this 'fail pattern' appears in the line...
+    if "- - - - -] Authorization failed" in line:
+        loginfail += 1 # this is the same as loginfail = loginfail + 1
+print("The number of failed log in attempts is", loginfail)
+keystone_file.close() # close the open file
+
+
---
+++ attemptlogin/large_fileparse-with.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/python3
+
+# parse keystone.common.wsgi and return number of failed login attempts
+loginfail = 0 # counter for fails
+
+successful_logins = 0 #start the count at 0 for successful logins
+
+# open the file for reading
+with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
+
+    # loop over the file
+    for line in kfile:
+        # if this 'fail pattern' appears in the line...
+        if "- - - - -] Authorization failed" in line:
+            loginfail += 1 # this is the same as loginfail = loginfail + 1
+        elif "-] Authorization failed" in line:
+            successful_logins += 1
+print("The number of failed log in attempts is", loginfail)
+print("The number of successful login attempts is ", successful_logins)
+
+
