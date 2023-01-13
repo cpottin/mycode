92ddab5071867a9cc42a7fdd56bd42d57662dc54
cpottin <cindy.pottin@gmail.com>
2023-01-13 22:58:11 +0000

    99 Bottles of Beer

---
+++ bottles/99bottlesofbeer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+for x in range(99, 0, -1):
+    print(x, "bottle of beer on the wall")
+    print(x, "bottles of beer, ")
+    print("Take one down, pass it around")
+    print()
+    print(x-1, "bottles of beer on the wall")
+    print("----------------------------")
+    print()
+
+
