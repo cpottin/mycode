37896d9e26f9d3f100300a5af1498c802c371dc9
cpottin <cindy.pottin@gmail.com>
2023-01-17 22:54:30 +0000

    Exploring how to work with large data sets

---
+++ iceAndFire04.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Alta3 Research - Exploring OpenAPIs with requests"""
+# documentation for this API is at
+# https://anapioficeandfire.com/Documentation
+
+import requests
+import pprint
+
+AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
+
+def main():
+        ## Ask user for input
+        got_charToLookup = input("Pick a number between 1 and 1000 to return info on a GoT character! " )
+
+        ## Send HTTPS GET to the API of ICE and Fire character resource
+        gotresp = requests.get(AOIF_CHAR + got_charToLookup)
+
+        ## Decode the response
+        got_dj = gotresp.json()
+        pprint.pprint(got_dj)
+
+if __name__ == "__main__":
+        main()
+
