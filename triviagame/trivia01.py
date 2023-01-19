570faf33020e9eac12c4b5a097081b6e76810121
cpottin <cindy.pottin@gmail.com>
2023-01-19 20:40:41 +0000

    update trivia game Chall 96

--- triviagame/trivia01.py
+++ triviagame/trivia01.py
@@ -9,22 +9,30 @@ URL= "https://opentdb.com/api.php?amount=10&category=27"
 
 def main():
     print("Are you ready to play a Trivia Game?")
-    start_game = input("Enter Y to start the game OR N to exit: ")
-
+    start_game = input("Would you like to answer a question, Press Y or N: ")
+    if(start_game == "Y"):
    # data will be a python dictionary rendered from your API link's JSON!
-    data= requests.get(URL).json()
-    print("Let's Begin")
-    time.sleep(1)
-    print()
-    print(data['results'][0]["question"])
-    print()
-    print("You have 3 seconds to guess the answer")
-    time.sleep(3)
-    print("1")
-    time.sleep(2)
-    print("2")
+        data= requests.get(URL).json()
+        print("Let's Begin")
+        time.sleep(1)
+        print()
+        print(data['results'][0]["question"])
+        print()
+        print("You have 3 seconds to guess the answer")
+        time.sleep(3)
+        print("1")
+        time.sleep(2)
+        print("2")
+        print()
+        user_answer = input("Please enter your answer : ")
 
-    print(data['results'][0]["correct_answer"])
+        if(user_answer == data['results'][0]['correct_answer']):
+            print("Great Job! You got it right")
+        else:
+            print("Better Luck next time, that was not correct")
+            print("The correct answer is : " , data['results'][0]["correct_answer"])
+    else:
+        print("Thank you")
 
 
 if __name__ == "__main__":
